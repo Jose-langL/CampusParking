d0f38244dae8d779a134fee862c988e96794857b
Jose-langL <totherrerajoseluis@gmail.com>
2026-05-14 08:49:40 -0600

    feat: :sparkles: crear función para eliminar
    Permite eliminar vehículos de la tabla.

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ const Usuarioprueba = "Admin1";
 const Contraprueba = "admin123";
 
 const validaUsuario = () => {
-    if(Usuario.value == Usuarioprueba && Contra.value == Contraprueba) {
+    if (Usuario.value == Usuarioprueba && Contra.value == Contraprueba) {
         window.location.href = "index.html";
         console.log("Usuario y contraseña correctos");
-    }else if (Usuario.value != Usuarioprueba && Contra.value == Contraprueba){
+    } else if (Usuario.value != Usuarioprueba && Contra.value == Contraprueba) {
         alert("Usuario incorrecto");
-    }else if (Usuario.value == Usuarioprueba && Contra.value != Contraprueba){
+    } else if (Usuario.value == Usuarioprueba && Contra.value != Contraprueba) {
         alert("Contraseña incorrecta");
-    }else{
+    } else {
         alert("Usuario y contraseña incorrectos");
     }
 }
@@ -25,45 +25,69 @@ formulariodatos.addEventListener("submit", validarformulario);
 
 //Variable Global
 const Vehiculos = JSON.parse(localStorage.getItem("Vehiculos")) || [];
-function validarformulario(e){
+
+function validarformulario(e) {
     e.preventDefault();
-    
+
     const Placa = document.getElementById("Placa").value;
     const TipoVehiculo = document.getElementById("TipoVehiculo").value;
     const FechaIngreso = document.getElementById("fechaingreso").value;
     const Slot = document.getElementById("Espacio").value;
 
-    nuevovehiculo = {Placa, TipoVehiculo, FechaIngreso, Slot};
+    nuevovehiculo = { Placa, TipoVehiculo, FechaIngreso, Slot };
 
     Vehiculos.push(nuevovehiculo);
     localStorage.setItem("Vehiculos", JSON.stringify(Vehiculos));
     formulariodatos.reset();
+    datostabla();
+    EliminarVehiculo();
 }
 
 //Funcion para poner datos de localhost en tabla
 const tabladatos = document.getElementById("tabla2")
 
-function datostabla(){
-   const listaVehiculos = Vehiculos
-   tabladatos.innerHTML = "";
-   listaVehiculos.forEach(datos => {
-    //Extraer datos de input fecha y hora
-      let fecha_hora = datos.FechaIngreso.split('T');
-      let fecha =  fecha_hora[0]
-      let hora = fecha_hora[1]
+function datostabla() {
+    const listaVehiculos = Vehiculos
+    tabladatos.innerHTML = "";
+    listaVehiculos.forEach(datos => {
+        //Extraer datos de input fecha y hora
+        let fecha_hora = datos.FechaIngreso.split('T');
+        let fecha = fecha_hora[0]
+        let hora = fecha_hora[1]
 
-       tabladatos.innerHTML +=`
-       <tr>
+        tabladatos.innerHTML += `
+        <tr>
             <td>${datos.Placa}</td>
             <td>${datos.TipoVehiculo}</td>
             <td>${fecha}</td>
             <td>${hora}</td>
             <td>${datos.Slot}</td>
-       </tr>
-       `
-   });
-   console.log(listaVehiculos)
-   
+            <td><button onclick="EliminarVehiculo()" class="btneliminar">Eliminar</button></td>
+            <td><button onclick="alert('Vehiculo Eliminado')">Modificar</button></td>
+        </tr>
+        `
+    });
 }
 
-datostabla()
+
+//Funcion para eliminar datos de la tabla y localstorage
+const tabla = document.getElementById("tabla1")
+
+function EliminarVehiculo(){
+    tabla1?.addEventListener('click', (e) => {
+    const btneliminar = e.target.closest('.btneliminar')
+    if(!btneliminar) return;
+    
+    const fila = btneliminar.closest('tr');
+    const placa = fila.querySelector('td:nth-child(1)').textContent;
+
+    if(!confirm('¿Desea eliminar este vehículo?')) return;
+
+    let eliminarvehiculo = JSON.parse(localStorage.getItem("Vehiculos")) || [];
+
+    eliminarvehiculo = eliminarvehiculo.filter(vehiculo => vehiculo.Placa !== placa);
+    localStorage.setItem("Vehiculos", JSON.stringify(eliminarvehiculo));
+    fila.remove();
+    });
+}
+
